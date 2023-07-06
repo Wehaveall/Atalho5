56b7951f0a20783d051095f27926410b54788f5e
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-07-06 19:42:43 -0300

    #myTable tr.selected {     background-color: #a8dadc; }

--- appfunctions.js
+++ appfunctions.js
@@ -1,24 +1,36 @@
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  var table = document.getElementById('myTable');
+
+  table.onclick = function (event) {
+    var target = event.target;
+    while (target && target.nodeName !== "TR") {
+      target = target.parentNode;
+    }
+    if (!target) { return; }
+
+    var selected = table.querySelector('.selected');
+    if (selected) {
+      selected.classList.remove('selected');
+    }
+
+    target.classList.add('selected');
+  }
+});
+
+
 
 
 
 function maximizeOrRestore() {
 
-    var button = document.getElementById("maxRestore");  // get the button
-    var icon = button.getElementsByTagName('img')[0];  // get the img element in the button
-  
-    // check the current icon and change it
-    if (icon.src.includes("maxBtn_white")) {
-     
-      icon.src = "/src/images/restoreBtn_white.png";  // change to restore icon
-    } else {
-     
-      icon.src = "/src/images/maxBtn_white.png";  // change back to maximize icon
-    }
-    
-
-    pywebview.api.maximize_or_restore_window();  // Let Python handle the maximizing/restoring
-
-    
+  var button = document.getElementById("maxRestore");  // get the button
+  var icon = button.getElementsByTagName('img')[0];  // get the img element in the button
+  // check the current icon and change it
+  if (icon.src.includes("maxBtn_white")) {
+    icon.src = "/src/images/restoreBtn_white.png";  // change to restore icon
+  } else {
+    icon.src = "/src/images/maxBtn_white.png";  // change back to maximize icon
   }
-  
+  pywebview.api.maximize_or_restore_window();  // Let Python handle the maximizing/restoring
+}
