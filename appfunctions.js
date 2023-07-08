6f3a7efabc1607396a4b0f518c6777f55169e05f
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-07-08 11:02:02 -0300

    left panel  colapsible

--- appfunctions.js
+++ appfunctions.js
@@ -69,3 +69,27 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+
+
+/////////////////////////////////////////////////////// LEFT PANEL
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      var arrow = this.getElementsByClassName('arrow')[0];
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        arrow.innerHTML = " ► ";
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        arrow.innerHTML = " ▼ ";
+      }
+    });
+  }
+});
+
