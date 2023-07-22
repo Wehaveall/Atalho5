340be642c2a93f40d992a5da378f6ddb40e943c6
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-07-22 16:48:10 -0300

    skin

--- richTextEditor.js
+++ richTextEditor.js
@@ -1,12 +1,14 @@
 window.onload = function () {
     tinymce.init({
         selector: '#editor',
+
         plugins: ['code'
 
         ],
+        statusbar: false,
         menubar: false,
         toolbar1: 'undo redo | blocks fontfamily fontsize',
         toolbar2: 'bold italic underline strikethrough | link image media table mergetags code | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-        content_css: 'tinymce/skins/content/default/content.min.css'
+        // content_css: 'tinymce/skins/content/default/content.min.css'
     });
 }
