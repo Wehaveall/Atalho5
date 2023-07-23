4b702d2b3ef23d710bdfc730b3f290a024f5b846
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-07-23 17:16:43 -0300

    very good

--- dataCreator.py
+++ dataCreator.py
@@ -11,6 +11,35 @@ from bs4 import BeautifulSoup
 # Function to extract fields from the text
 # Function to extract fields from the text
 # Function to extract fields from the text
+# Function to convert a Roman numeral to a decimal number
+def roman_to_decimal(roman):
+    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    total = 0
+    prev_value = 0
+    for char in reversed(roman):
+        value = values[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
+
+
+# Function to extract fields from the text
+# Function to extract fields from the text
+# Function to extract fields from the text
+# Function to extract fields from the text
+# Function to extract fields from the text
+def extract_fields(html, prefix):
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text("\n")
+    pattern = re.compile(r"\*")
+    articles = pattern.split(text)
+    fields = []
+
+
+# Function to extract fields from the text
 def extract_fields(html, prefix):
     soup = BeautifulSoup(html, "html.parser")
     text = soup.get_text("\n")
@@ -27,6 +56,28 @@ def extract_fields(html, prefix):
             expansion = "*" + article
             fields.append((prefix, shortcut, expansion))
 
+            # Split the article into incisos at the '$' delimiter
+            incisos = re.split(r"\$", expansion)
+
+            for k, inciso in enumerate(
+                incisos, start=0
+            ):  # We start the enumeration at 0
+                if k == 0:  # Skip the first inciso because it's already added
+                    continue
+
+                # The 'expansion' of the new row should be the text of the inciso
+                new_expansion = "*" + inciso.strip()
+
+                # Extract the Roman numeral after the '$' delimiter and convert it to a decimal number
+                roman_numeral = re.search(r"[IVXLCDM]+", inciso)
+                if roman_numeral:
+                    decimal_number = roman_to_decimal(roman_numeral.group())
+
+                    # Create a new shortcut for the inciso with 'i' suffix and the inciso number
+                    new_shortcut_inciso = shortcut + "i" + str(decimal_number)
+
+                    fields.append((prefix, new_shortcut_inciso, new_expansion))
+
             # Check if '#' delimiter is present in the 'expansion'
             if "#" in expansion:
                 # Split the 'expansion' into two parts at the '#' delimiter
@@ -40,10 +91,32 @@ def extract_fields(html, prefix):
 
                 fields.append((prefix, new_shortcut, new_expansion))
 
+                # Split the expansion into incisos at the '$' delimiter
+                incisos = re.split(r"\$", new_expansion)
+
+                for k, inciso in enumerate(
+                    incisos, start=0
+                ):  # We start the enumeration at 0
+                    if k == 0:  # Skip the first inciso because it's already added
+                        continue
+
+                    # The 'expansion' of the new row should be the text of the inciso
+                    new_expansion = "*" + inciso.strip()
+
+                    # Extract the Roman numeral after the '$' delimiter and convert it to a decimal number
+                    roman_numeral = re.search(r"[IVXLCDM]+", inciso)
+                    if roman_numeral:
+                        decimal_number = roman_to_decimal(roman_numeral.group())
+
+                        # Create a new shortcut for the inciso with 'i' suffix and the inciso number
+                        new_shortcut_inciso = new_shortcut + "i" + str(decimal_number)
+
+                        fields.append((prefix, new_shortcut_inciso, new_expansion))
+
             # Split the article into paragraphs at the '%' delimiter
             paragraphs = re.split(r"%", article)
 
-            for j, paragraph in enumerate(paragraphs):
+            for j, paragraph in enumerate(paragraphs, start=0):
                 if j == 0:  # Skip the first paragraph because it's already added
                     continue
 
@@ -54,6 +127,28 @@ def extract_fields(html, prefix):
                 new_shortcut = shortcut + "p" + str(j)
 
                 fields.append((prefix, new_shortcut, new_expansion))
+
+                # Split the paragraph into incisos at the '$' delimiter
+                incisos = re.split(r"\$", new_expansion)
+
+                for k, inciso in enumerate(
+                    incisos, start=0
+                ):  # We start the enumeration at 0
+                    if k == 0:  # Skip the first inciso because it's already added
+                        continue
+
+                    # The 'expansion' of the new row should be the text of the inciso
+                    new_expansion = "*" + inciso.strip()
+
+                    # Extract the Roman numeral after the '$' delimiter and convert it to a decimal number
+                    roman_numeral = re.search(r"[IVXLCDM]+", inciso)
+                    if roman_numeral:
+                        decimal_number = roman_to_decimal(roman_numeral.group())
+
+                        # Create a new shortcut for the inciso with 'i' suffix and the inciso number
+                        new_shortcut_inciso = new_shortcut + "i" + str(decimal_number)
+
+                        fields.append((prefix, new_shortcut_inciso, new_expansion))
 
     return fields
 
