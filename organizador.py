0717b9ffa8787c4fe07ad9458889749cafd2c607
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-08-11 15:44:37 -0300

    organizar +

--- organizador.py
+++ organizador.py
@@ -1,51 +1,49 @@
 from bs4 import BeautifulSoup
 import re
 
+def process_element(element):
+    # Inicializa o texto vazio
+    text = ""
+    
+    # Itera sobre os elementos filhos, incluindo texto e elementos HTML
+    for child in element.children:
+        if isinstance(child, str): # Se for um texto, apenas o adiciona
+            text += child.strip() + " "
+        else: # Se for um elemento HTML, processa-o recursivamente
+            text += process_element(child) + " "
+
+    # Substituir ocorrências de "1o", "2 o", etc., por "1º", "2º", etc.
+    text = re.sub(r"(\d+)\s*o", r"\1º", text)
+
+    # Remove qualquer sequência de espaços em branco (incluindo tabulações e novas linhas)
+    text = re.sub(r"\s+", " ", text)
+
+    return text.strip() # Remove espaços em branco extras
 
 def modify_legal_document(input_file_path, output_file_path):
-    # Read the input file
     with open(input_file_path, "r", encoding="ISO-8859-1") as file:
         content = file.read()
 
-    # Parse the HTML content
     soup = BeautifulSoup(content, "html.parser")
-
-    # Define the start of each type of element
     article_start = "Art."
     paragraph_start = ("§", "Parágrafo único")
-
-    # Define regular expressions for items and subitems
     item_pattern = re.compile(r"^(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}) ?-")
     subitem_pattern = re.compile(r"^[a-z]\)")
 
-    # Initialize variables to handle lines and heading
     modified_lines = []
-    insert_star = False
-    first_star = True
-    last_centered = False
 
-    for element in soup.descendants:
-        if element.name == "p" and element.string:
-            text = element.string.strip()
-
-            # Check if the text is centered or uppercase
-            if element.attrs.get("align") == "center" or text.isupper():
-                if not first_star and not last_centered:
-                    text = "*" + text
-                    last_centered = True
-                else:
-                    last_centered = False
-                    text = text.lstrip(
-                        "*"
-                    )  # Remove any asterisks from consecutive centered uppercase lines
-
-                insert_star = True
+    for element in soup.find_all(True, recursive=True):  # Loop through all elements
+        if element.name == "p" or (
+            element.name == "font" and "Artart" in element.get("class", [])
+        ):
+            text = process_element(element)
 
             if text.startswith(article_start):
-                if first_star or not insert_star:
-                    text = "*" + text
-                first_star = False
-                insert_star = False
+                text = "*" + text
+
+            if element.attrs.get("align") == "center" or text.isupper():
+                if text.startswith("*"):
+                    text = text[1:]
 
             if text.startswith(article_start):
                 modified_lines.append(text)
@@ -64,6 +62,24 @@ def modify_legal_document(input_file_path, output_file_path):
             else:
                 modified_lines.append(text)
 
+    # Find the index of the first "*" and remove everything before it
+    first_star_index = next(
+        idx for idx, line in enumerate(modified_lines) if line.startswith("*")
+    )
+    modified_lines = modified_lines[first_star_index:]
+
+    # Find the index of the last "*"
+    last_star_index = max(
+        idx for idx, line in enumerate(modified_lines) if line.startswith("*")
+    )
+
+    # Find the text following "Brasília" after the last "*"
+    for i in range(last_star_index, len(modified_lines)):
+        line = modified_lines[i]
+        if re.search(r"Bras[ií]lia", line, re.IGNORECASE):
+            modified_lines = modified_lines[: i + 1]
+            break
+
     # Join the modified lines into a single text
     modified_text = "\n".join(modified_lines)
 
@@ -71,6 +87,5 @@ def modify_legal_document(input_file_path, output_file_path):
     with open(output_file_path, "w", encoding="utf-8") as file:
         file.write(modified_text)
 
-
 # Test the function with the original file and a new output file
-modify_legal_document("E:/ce.html", "E:/ce_v2.txt")
+modify_legal_document("E:/cc.html", "E:/cc_v2.txt")
