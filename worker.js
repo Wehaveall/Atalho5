7d938d5edacb5bd66070cfc94c8c84d2aaa84cce
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-10-08 13:44:55 -0300

    more fixes

--- worker.js
+++ worker.js
@@ -2,7 +2,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
 self.addEventListener('message', async function (e) {
     const totalRows = e.data;  // We're sending only the length of data
     let processedRows = 0;
@@ -10,13 +9,11 @@ self.addEventListener('message', async function (e) {
     for (let index = 0; index < totalRows; index++) {
         processedRows++;
 
-        // Introduce a 1ms delay every 100 rows
-        if (processedRows % 100 === 0) {
+        // Introduce a 1ms delay every 200 rows
+        if (processedRows % 200 === 0) {
             await sleep(1);
         }
-
         const progress = Math.round((processedRows / totalRows) * 100);
-
         // Post progress back to main thread
         self.postMessage(progress);
     }
