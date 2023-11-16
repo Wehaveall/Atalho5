1772f389aec1276ebe049f34074671dca684fb01
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-11-16 10:00:10 -0300

    best ident until now. All looks solved. test  more

--- identifier.py
+++ identifier.py
@@ -14,16 +14,16 @@ from comtypes.gen.UIAutomationClient import (
     TreeScope_Children,
     UIA_ProcessIdPropertyId,
     UIA_NamePropertyId,
+    TextUnit_Word,
     TextPatternRangeEndpoint_Start,
     TextPatternRangeEndpoint_End,
-    TextUnit_Character,
-    TextUnit_Word,
-    TextUnit_Document,
+    UIA_ValuePatternId,
+    IUIAutomationValuePattern,
+    TextUnit_Character,  # Add this to your imports
 )
 
 
 def get_app_name_from_element(element, uiautomation_client):
-    # Get process ID and find the top-level window
     process_id = element.GetCurrentPropertyValue(UIA_ProcessIdPropertyId)
     condition = uiautomation_client.CreatePropertyCondition(
         UIA_ProcessIdPropertyId, process_id
@@ -35,6 +35,44 @@ def get_app_name_from_element(element, uiautomation_client):
         if app_window
         else "Unknown"
     )
+
+
+def get_word_at_caret(textPattern):
+    selection = textPattern.GetSelection()
+    if selection.length > 0:
+        range = selection.GetElement(0).clone()
+        # Collapse the range to a degenerate (empty) range at the start
+        range.MoveEndpointByRange(
+            TextPatternRangeEndpoint_End, range, TextPatternRangeEndpoint_Start
+        )
+        # Move the start of the range backward by one word
+        range.MoveEndpointByUnit(TextPatternRangeEndpoint_Start, TextUnit_Word, -1)
+        # Move the end of the range forward by one word
+        range.MoveEndpointByUnit(TextPatternRangeEndpoint_End, TextUnit_Word, 1)
+        # Get the text of the range, which should now be just the word at the caret
+        word = range.GetText(-1).strip()
+        # If the word contains spaces, it might be more than one word,
+        # so we take the first part that is likely the word at the caret
+        word_parts = word.split()
+        return word_parts[0] if word_parts else ""
+    return ""
+
+
+def get_full_text(element, textPattern):
+    # First attempt to use the TextPattern's DocumentRange to get the full text
+    documentRange = textPattern.DocumentRange
+    if documentRange:
+        full_text = documentRange.GetText(-1)
+        if full_text:
+            return full_text.strip()
+
+    # If the TextPattern doesn't provide the full text, use the ValuePattern
+    valuePattern = element.GetCurrentPattern(UIA_ValuePatternId)
+    if valuePattern:
+        valuePattern = valuePattern.QueryInterface(IUIAutomationValuePattern)
+        return valuePattern.CurrentValue.strip()
+
+    return ""
 
 
 def get_focused_info():
@@ -51,28 +89,19 @@ def get_focused_info():
             print("No focused element.")
             return {}
 
+        app_name = get_app_name_from_element(focusedElement, uiautomation_client)
         info = {
-            "app_name": get_app_name_from_element(focusedElement, uiautomation_client),
+            "app_name": app_name,
             "element_name": focusedElement.CurrentName,
-            "full_text": "",
             "word_at_caret": "",
+            "full_text": "",
         }
 
         textPattern = focusedElement.GetCurrentPattern(UIA_TextPatternId)
         if textPattern:
             textPattern = textPattern.QueryInterface(IUIAutomationTextPattern)
-            range = textPattern.GetSelection().GetElement(0)
-            if range:
-                range.ExpandToEnclosingUnit(TextUnit_Word)
-                word_at_caret = range.GetText(-1).strip()
-                info["word_at_caret"] = "".join(filter(str.isalnum, word_at_caret))
-
-        textPattern = focusedElement.GetCurrentPattern(UIA_TextPatternId)
-        if textPattern:
-            textPattern = textPattern.QueryInterface(IUIAutomationTextPattern)
-            documentRange = textPattern.DocumentRange
-            if documentRange:
-                info["full_text"] = documentRange.GetText(-1)
+            info["word_at_caret"] = get_word_at_caret(textPattern)
+            info["full_text"] = get_full_text(focusedElement, textPattern)
 
         return info
 
