04b79a5a7c947cfd2a21c9d888dcd5457ea31fde
Wehaveall <86645021+Wehaveall@users.noreply.github.com>
2023-12-01 14:32:31 -0300

    Looks lite its done - test more

--- src/classes/class_Select_change.js
+++ src/classes/class_Select_change.js
@@ -8,7 +8,6 @@ window.customSelects = {};
 
 // Initialize flag for manual change detection
 let isManualChange = false;
-
 document.addEventListener("DOMContentLoaded", () => {
   const selectElements = document.querySelectorAll("[data-custom]");
 
@@ -17,7 +16,6 @@ document.addEventListener("DOMContentLoaded", () => {
     window.customSelects[selectElement.id] = customSelect;
 
     customSelect.customElement.addEventListener('valueSelected', function (event) {
-
       const selectedValue = event.detail.value;
 
       if (isEditorUpdate || !window.currentRow) {
@@ -25,56 +23,72 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       const shortcut = window.currentRow.dataset.shortcut;
-      const groupName = window.currentRow.dataset.groupName; 
+      const groupName = window.currentRow.dataset.groupName;
+      const databaseName = window.currentRow.dataset.databaseName;
       const tableName = window.currentRow.dataset.tableName;
       const indexValue = window.currentRow.dataset.indexValue;
-      const databaseName = window.currentRow.dataset.databaseName;
-      const currentContent = tinyMCE.get('editor').getContent();
       const label = window.currentRow.dataset.label;
       let formatValue;
       let caseChoice;
 
       if (selectElement.id === "escolha") {
         formatValue = selectedValue === "1";
-        caseChoice = window.currentRow.dataset.caseChoice; // Get the existing value
+        caseChoice = window.currentRow.dataset.caseChoice;
 
-        // Check if format dropdown changed to '0' and remove formatting
+        var editor = tinyMCE.get('editor');
         if (formatValue === false) {
-          var editor = tinyMCE.get('editor');
+          // Display plain text, but keep formatted content in database
           var plainTextContent = editor.getBody().textContent || editor.getBody().innerText;
-          editor.setContent(plainTextContent);
-          editor.execCommand('RemoveFormat');
+          editor.setContent(plainTextContent, { format: 'text' });
+        } else {
+          // Refetch and display the original formatted content
+          refetchAndDisplayFormattedContent(groupName, databaseName, tableName, indexValue);
         }
-        
+
         reinitializeEditor(selectedValue);
       } else if (selectElement.id === "caseChoice") {
         caseChoice = selectedValue;
-        window.currentRow.dataset.caseChoice = caseChoice; // Store the new value
+        window.currentRow.dataset.caseChoice = caseChoice;
       }
-      //alert("Format Value being sent: " + formatValue + ", Type: " + typeof formatValue);
 
-      // Set the flag to true because this change was made by the user
       isManualChange = true;
 
       if (isManualChange) { // Only save if changed manually
         isSaving = true;
-        window.pywebview.api.save_changes(groupName, databaseName, tableName, indexValue, shortcut, currentContent, formatValue || null, label || null, caseChoice || null)
+        const contentToSave = formatValue ? editor.getContent() : window.currentRow.dataset.expansion;
+
+        window.pywebview.api.save_changes(groupName, databaseName, tableName, indexValue, shortcut, contentToSave, formatValue || null, label || null, caseChoice || null)
           .then(response => {
-            if (formatValue !== undefined) {
-              window.currentRow.dataset.format = formatValue ? 'true' : 'false';
-            }
+            window.currentRow.dataset.format = formatValue ? 'true' : 'false';
             if (caseChoice !== undefined) {
-              window.currentRow.dataset.caseChoice = caseChoice; // Store the updated value
+              window.currentRow.dataset.caseChoice = caseChoice;
             }
-            isSaving = false;
-      
-            // Reset the flag after making the changes
-            isManualChange = false;
           })
           .catch((error) => {
+            console.error('Error:', error);
+          })
+          .finally(() => {
+            // Ensure isSaving is reset after the operation completes, regardless of success or failure
             isSaving = false;
+            isManualChange = false;
           });
       }
     });
   });
 });
+
+function refetchAndDisplayFormattedContent(groupName, databaseName, tableName, indexValue) {
+  window.pywebview.api.get_data(groupName, databaseName, tableName)
+    .then(data => {
+      const rowData = data.find(row => row.id.toString() === indexValue);
+      if (rowData) {
+        const editor = tinyMCE.get('editor');
+        editor.setContent(rowData.expansion);  // Set the original formatted content
+      } else {
+        alert('No data found for the selected row with id: ' + indexValue);
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+}
